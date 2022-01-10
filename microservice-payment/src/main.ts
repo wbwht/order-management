96ac63f041cf1e23f4011c17243a3bffbaf02b85
Weng Yi <tamwengyi@gmail.com>
2022-01-10 22:21:01 +0800

    Updated host name to work on Kubernetes

--- microservice-payment/src/main.ts
+++ microservice-payment/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     {
       transport: Transport.TCP,
       options: {
-        host: 'payment', //'127.0.0.1', //'payment',
+        host: '0.0.0.0', //'payment', //'127.0.0.1', //'payment',
         port: 8080,
       },
     },
